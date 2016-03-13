3f05405e34c7ccef4bd4f8f83239aa2eb6c90038
sevenlist <sevenlist@web.de>
2016-03-13 11:14:36 +0100

    New.

---
+++ src/main/groovy/com/sevenlist/springtodo/SpringTodoApplication.groovy
@@ -0,0 +1,12 @@
+package com.sevenlist.springtodo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringTodoApplication {
+
+    static main(args) {
+        SpringApplication.run(SpringTodoApplication, args)
+    }
+}
